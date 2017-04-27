94326a409d41325509a5e579cecd18ff926cb610
Rishats <sultanov-rishat@inbox.ru>
2017-04-27 20:34:32 +0600

    Add frontend. (Bootstrap 3) Add view with FORM(Demo) with Form facade. Add dump sql file, where created 1 table.

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -2,9 +2,53 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <title>Барашки</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/add.css" rel="stylesheet">
+    <title>Овечки</title>
 </head>
 <body>
-
+<!-- Загон первый -->
+<div class="divzagons">
+    <h1>Фермочка для овечек</h1>
+    </br></br>
+<div class="form-group">
+    {!! Form::label('multipleselect[]', 'Загон 1', ['class' => 'col-lg-2 control-label'] )  !!}
+    <div class="col-lg-10">
+        {!!  Form::select('multipleselect[]', ['honda' => 'Honda', 'toyota' => 'Toyota', 'subaru' => 'Subaru', 'ford' => 'Ford', 'nissan' => 'Nissan'], $selected = null, ['class' => 'form-control', 'multiple' => 'multiple']) !!}
+    </div>
+</div>
+<!-- Загон второй -->
+    </br></br></br></br>
+<div class="form-group">
+    {!! Form::label('multipleselect[]', 'Загон 2', ['class' => 'col-lg-2 control-label'] )  !!}
+    <div class="col-lg-10">
+        {!!  Form::select('multipleselect[]', ['honda' => 'Honda', 'toyota' => 'Toyota', 'subaru' => 'Subaru', 'ford' => 'Ford', 'nissan' => 'Nissan'], $selected = null, ['class' => 'form-control', 'multiple' => 'multiple']) !!}
+    </div>
+</div>
+<!-- Загон третий -->
+    </br></br></br></br>
+<div class="form-group">
+    {!! Form::label('multipleselect[]', 'Загон 3', ['class' => 'col-lg-2 control-label'] )  !!}
+    <div class="col-lg-10">
+        {!!  Form::select('multipleselect[]', ['honda' => 'Honda', 'toyota' => 'Toyota', 'subaru' => 'Subaru', 'ford' => 'Ford', 'nissan' => 'Nissan'], $selected = null, ['class' => 'form-control', 'multiple' => 'multiple']) !!}
+    </div>
+</div>
+<!-- Загон четвертый -->
+    </br></br></br></br>
+<div class="form-group">
+    {!! Form::label('multipleselect[]', 'Загон 4', ['class' => 'col-lg-2 control-label'] )  !!}
+    <div class="col-lg-10">
+        {!!  Form::select('multipleselect[]', ['honda' => 'Honda', 'toyota' => 'Toyota', 'subaru' => 'Subaru', 'ford' => 'Ford', 'nissan' => 'Nissan'], $selected = null, ['class' => 'form-control', 'multiple' => 'multiple']) !!}
+    </div>
+</div>
+    </br></br></br></br>
+</div>
+</br></br>
+<div class="button1">
+    <button type="button" class="btn btn-success">Обновить</button>
+</div>
+<div class="button2">
+    <button type="button" class="btn btn-danger">Зарубить овечек</button>
+</div>
 </body>
 </html>
